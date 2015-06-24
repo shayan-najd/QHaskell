de205c6a3909af1dd3a039712664fb9f896fb17f
Shayan Najd <sh.najd@gmail.com>
2015-06-24 06:00:34 +0100

    Reseting for the new version

--- QHaskell/Expression/Utils/Show/GADTFirstOrder.hs
+++
@@ -1,7 +0,0 @@
-module QHaskell.Expression.Utils.Show.GADTFirstOrder
-       () where
-
-import QHaskell.MyPrelude
-import QHaskell.Expression.GADTFirstOrder as FGFO
-
-deriving instance Show (Exp g a)
--- QHaskell/Variable/Plain.hs
+++
@@ -1,6 +0,0 @@
-module QHaskell.Variable.Plain
-  (Var,Nat(..),prd,inc) where
-
-import QHaskell.Nat.ADT
-
-type Var = Nat
